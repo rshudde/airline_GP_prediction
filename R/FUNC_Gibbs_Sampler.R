f58a02b063c34ad5685079a3ccefdc5d3fc75e4c
rshudde <rachael.shudde@gmail.com>
2020-09-14 15:35:33 -0500

    changed vraibles names to match data generation

--- R/FUNC_Gibbs_Sampler.R
+++ R/FUNC_Gibbs_Sampler.R
@@ -4,32 +4,14 @@ library(invgamma)
 library(MASS)
 source('~/Desktop/Summer2020/AirplanePaper/airline_GP_prediction/R/FUNC_woodchan_samples.R')
 source('~/Desktop/Summer2020/AirplanePaper/airline_GP_prediction/R/FUNC_paramater_estimates.R')
-
+# source('~/Desktop/Summer2020/AirplanePaper/airline_GP_prediction/R/DATA_generate_simulation.R')
 # initial estimates for paramaters outside loop
 
 
 # get data
-nrow = 50
-ncol = 20
-data = list()
-y = vector()
-
-n_datasets = 40
-for (i in 1:n_datasets)
-{
-  mean = sample(1:10, 1)
-  sd = sample(2:9, 1)
-  
-  temp_x = normalize_data(matrix(rnorm(nrow*ncol, mean, sd), nrow, ncol))
-  
-  min_y = sample(1:1000, 1)
-  max_y = sample(1:1000, 1)
-  temp_y = sample(min_y:max_y, nrow, replace = TRUE)
-  
-  data[[i]] = temp_x
-  y = rbind(y, temp_y)
-}
-
+data = generate_simulation_data(50, 10)
+X = data$X
+y = data$y
 
 # initialize hyperparamaters
 sigma_mu = 2
@@ -41,7 +23,7 @@ a = 0.1
 b = 0.1
 
 # initialize paramaters
-alpha_0 = rep(1, ncol(data[[1]]))
+alpha_0 = rep(1, ncol(X[[1]]))
 xi_0 = rnorm(n) # length of knots
 mu_0 = rep(0, nrow(y)) # length is number of flights
 sigma_2_0 = 1
@@ -63,14 +45,14 @@ g = list()
 
 for (i in 1:nrow(y))
 {
-  M[[i]] = get_matern(l_k_0, y[i, ])
+  M[[i]] = get_matern(l_k_0, rownames(y[i, ]))
   K[[i]] = get_K_i(sigma_2_0, M[[i]])
   V[[i]] = get_V_i(sigma_2_0, M[[i]], K[[i]])
 }
 # initial iteration outside the loop
 
 # getting beta
-beta_i = get_beta(alpha_0, y, mu_0, data, xi_0, knots, N, sigma_2_0, l_k, M, K)
+beta_i = get_beta(alpha_0, y, mu_0, X, xi_0, knots, N, sigma_2_0, l_k_0, M, K)
 beta = rbind(beta, beta_i)
 
 # getting mu
@@ -78,7 +60,7 @@ mu_temp = vector()
 g = list()
 for (i in 1:nrow(y))
 {
-  g[[i]] = get_g(data[[i]], beta_i, knots, N, xi_0)
+  g[[i]] = get_g(X[[i]], beta_i, knots, N, xi_0)
   sigma_mu_post_temp = get_sigma_mu_post(sigma_2_0, sigma_mu, V[[i]])
   alpha_mu_post_temp = get_alpha_mu_post(alpha_mu, sigma_mu, sigma_mu_post_temp, g[[i]], V[[i]], y[i,])
   mu_temp[i] = get_mu_i(alpha_mu_post_temp, sigma_mu_post_temp)
@@ -89,7 +71,7 @@ mu = rbind(mu, mu_temp)
 sigma_2[1] = get_sigma_squared(a, b, y, M, mu, g)
 
 # getting xi 
-xi_temp = get_xi(xi_0, y, mu, data, beta_i, knots, N, sigma_2[1], l_k_0, l_b, M, K)
+xi_temp = get_xi(xi_0, y, mu, X, beta_i, knots, N, sigma_2[1], l_k_0, l_b, M, K)
 xi = rbind(xi, xi_temp)
   
 # get l_k
@@ -97,7 +79,7 @@ l_k[1] = get_lk(y, mu, g, sigma_2[1], l_k_0)
 
 
 # now loop over everything
-B = 500
+B = 10000
 for (b in 2:B)
 {
   # updating M and K
@@ -109,7 +91,7 @@ for (b in 2:B)
   }
   
   # getting beta
-  beta_i = get_beta(alpha_0, y, mu[b-1, ], data, xi[b-1, ], knots, N, sigma_2[b-1], l_k[b-1], M, K)
+  beta_i = get_beta(alpha_0, y, mu[b-1, ], X, xi[b-1, ], knots, N, sigma_2[b-1], l_k[b-1], M, K)
   beta = rbind(beta, beta_i)
   
   # getting mu
@@ -117,7 +99,7 @@ for (b in 2:B)
   g = list()
   for (i in 1:nrow(y))
   {
-    g[[i]] = get_g(data[[i]], beta[b, ], knots, N, xi[b-1, ])
+    g[[i]] = get_g(X[[i]], beta[b, ], knots, N, xi[b-1, ])
     sigma_mu_post_temp = get_sigma_mu_post(sigma_2[b-1], sigma_mu, V[[i]])
     alpha_mu_post_temp = get_alpha_mu_post(alpha_mu, sigma_mu, sigma_mu_post_temp, g[[i]], V[[i]], y[i,])
     mu_temp[i] = get_mu_i(alpha_mu_post_temp, abs(sigma_mu_post_temp))
@@ -129,7 +111,7 @@ for (b in 2:B)
   sigma_2[b] = get_sigma_squared(a, b, y, M, mu[b, ], g)
   
   # getting xi 
-  xi_temp = get_xi(xi[b-1,], y, mu[b, ], data, beta[b, ], knots, N, sigma_2[b], l_k[b-1], l_b, M, K)
+  xi_temp = get_xi(xi[b-1,], y, mu[b, ], X, beta[b, ], knots, N, sigma_2[b], l_k[b-1], l_b, M, K)
   xi = rbind(xi, xi_temp)
   
   # get l_k
