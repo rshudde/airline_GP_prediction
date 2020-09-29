b3ee1e06591c790b38ef456ae1a68977219c8d8e
rshudde <rachael.shudde@gmail.com>
2020-09-29 14:56:36 -0500

    re-copied pallavi's code to make sure for samp.wc l value was correct

--- R/FUNC_woodchan_samples.R
+++ R/FUNC_woodchan_samples.R
@@ -50,53 +50,47 @@ circ_vec = function(knot, g, nu, l, tausq)
 }
 
 # Function for finding a g such that C is nnd:
-eig.eval = function(knot, g, nu, l, tausq)
-{
-  vec = circ_vec(knot, g, nu, l, tausq)
-  C = circulant(vec)
-  ev = min(eigen(C)$values)
-  return(list("vec" = vec,  "min.eig.val" = ev))
+eig.eval=function(knot,g,nu,l,tausq){
+  vec=circ_vec(knot,g,nu,l,tausq)
+  C=circulant(vec)
+  ev=min(eigen(C)$values)
+  return(list("vec" = vec, "min.eig.val" = ev))
 }
 
 # Function for finding a g such that C is nnd:
 # without forming the circulant matrix and without computing eigen values:
-C.eval = function(knot, g, nu, l, tausq)
-{
-  vec = circ_vec(knot, g, nu, l, tausq)
-  val = fft(vec) # eigenvalues will be real as the circulant matrix formed by the 
+C.eval=function(knot,g,nu,l,tausq){
+  vec=circ_vec(knot,g,nu,l,tausq)
+  val=fft(vec) # eigenvalues will be real as the circulant matrix formed by the 
   # vector is by construction is symmetric!
-  ev = min(Re(val))
-  return(list("vec" = vec,  "min.eig.val" = ev))
+  ev=min(Re(val))
+  return(list("vec" = vec, "min.eig.val" = ev))
 }
 
 
-nnd_C = function(knot, g, nu, l, tausq)
-{
-  C.vec = C.eval(knot, g, nu, l, tausq)$vec
-  eval = C.eval(knot, g, nu, l, tausq)$min.eig.val
-  # print(eval)
-  if (eval > 0) 
-  {
-    return(list("cj" = C.vec, "g" = g))
-  }
+nnd_C=function(knot,g,nu,l,tausq){
+  C.vec=C.eval(knot,g,nu,l,tausq)$vec
+  eval=C.eval(knot,g,nu,l,tausq)$min.eig.val
+  if(eval>0)
+    return(list("cj" = C.vec,"g" = g))
   else{
-    # print("here")
-    g = g + 1
-    nnd_C(knot, g, nu, l, tausq)
+    g=g+1
+    nnd_C(knot,g,nu,l,tausq)
   }
 }
+
 
 # computing the eigen values of C using FFT:
-eigval = function(knot, nu, l, tausq)
-{
-  g = min_g(knot)
-  c.j = nnd_C(knot, g, nu, l, tausq)$cj
-  lambda = Re(fft(c.j))
-  if (min(lambda) > 0) 
+eigval=function(knot,nu,l,tausq){
+  g=min_g(knot)
+  c.j=nnd_C(knot,g,nu,l,tausq)$cj
+  lambda=Re(fft(c.j))
+  if(min(lambda)>0)
     return(lambda)
   else
     stop("nnd condition is NOT satisfied!!!")
 }
+
 
 #################################################################
 ########## Samples drawn using Wood and Chan Algorithm ##########
@@ -104,26 +98,23 @@ eigval = function(knot, nu, l, tausq)
 # N: (N + 1) is the number of knots
 # nu:smoothness parameter of Matern
 # l:length - scale parameter of Matern
-samp.WC = function(knot, l, nu = 5/2, tausq = 1)
-{
-  N = length(knot)
-  lambda = eigval(knot, nu, l, tausq)
-  m = length(lambda)
-  samp.vec = rep(0, N)
-  a = rep(0, m)
-  a[1] = sqrt(lambda[1]) * rnorm(1) / sqrt(m)
-  a[(m / 2) + 1] = sqrt(lambda[(m / 2) + 1]) * rnorm(1) / sqrt(m)
-  i = sqrt(as.complex( - 1))
-  for (j in 2:(m / 2))
-  {
-    uj = rnorm(1); vj = rnorm(1)
-    a[j] = (sqrt(lambda[j]) * (uj  +  i * vj)) / (sqrt(2 * m))
-    a[m + 2 - j] = (sqrt(lambda[j]) * (uj  -  i * vj)) / (sqrt(2 * m))
+samp.WC=function(knot, l, nu = 5/2, tausq = 1){
+  N=length(knot)
+  lambda=eigval(knot,nu,l,tausq)
+  m=length(lambda)
+  samp.vec=rep(0,N)
+  a=rep(0,m)
+  a[1]=sqrt(lambda[1])*rnorm(1)/sqrt(m)
+  a[(m/2)+1]=sqrt(lambda[(m/2)+1])*rnorm(1)/sqrt(m)
+  i=sqrt(as.complex(-1))
+  for(j in 2:(m/2)){
+    uj=rnorm(1); vj=rnorm(1)
+    a[j]=(sqrt(lambda[j])*(uj + i*vj))/(sqrt(2*m))
+    a[m+2-j]=(sqrt(lambda[j])*(uj - i*vj))/(sqrt(2*m))
   }
-  samp = fft(a)
-  samp.vec = Re(samp[1:N])
+  samp=fft(a)
+  samp.vec=Re(samp[1:N])
   return(samp.vec)
 }
-
 
 
