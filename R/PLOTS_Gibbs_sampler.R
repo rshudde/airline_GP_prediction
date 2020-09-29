574efedb2a818eb5619e90a6b8743fcbc3e7e422
rshudde <rachael.shudde@gmail.com>
2020-09-29 14:56:46 -0500

    updated lb and lk starting values

--- R/PLOTS_Gibbs_sampler.R
+++ R/PLOTS_Gibbs_sampler.R
@@ -5,16 +5,16 @@ source('R/DATA_generate_simulation.R')
 source('R/FUNC_Gibbs_Sampler.R')
 
 # initial estimates for paramaters outside loop
-
-knots = seq(0, 1, length.out = 10) # suggested to be 
-
-
-# get data
-n_covariates = 10
-n_datasets = 100
-data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covariates, knots = knots)
-
-results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 500)
+# 
+# knots = seq(0, 1, length.out = 10) # suggested to be 
+# 
+# 
+# # get data
+# n_covariates = 10
+# n_datasets = 100
+# data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covariates, knots = knots)
+# 
+# results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 500)
 
 ###################
 beta_true = data$beta
