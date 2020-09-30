a548e72103dfcd1963b200b6a76030f0cf9a72bd
rshudde <rachael.shudde@gmail.com>
2020-09-30 16:23:24 -0500

    changed paramaters

--- R/main.R
+++ R/main.R
@@ -6,8 +6,8 @@ source('R/FUNC_Gibbs_Sampler.R')
 
 # initial estimates for paramaters outside loop
 # get data
-n_covariates = 10
-n_datasets = 10
+n_covariates = 15
+n_datasets = 500
 length_out = floor(n_covariates*n_datasets / 3)
 
 knots = seq(0, 1, length.out = length_out) # suggested to be n_covariates*n_datasets / 2
@@ -16,7 +16,7 @@ knots = seq(0, 1, length.out = length_out) # suggested to be n_covariates*n_data
 data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covariates, knots = knots)
 
 # run gibbs sampler
-results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 1000, write = TRUE, lb_0 = 0.1, lk_0 = 0.1)
+results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 5000, write = TRUE, lb_0 = 0.1, lk_0 = 0.1)
 
 
 
