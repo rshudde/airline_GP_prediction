3be79f2ffd0426ebf6cbda48f1b69e354b8955e1
rshudde <rachael.shudde@gmail.com>
2020-10-01 16:16:26 -0500

    removed legend from plots

--- R/PLOTS_Gibbs_sampler.R
+++ R/PLOTS_Gibbs_sampler.R
@@ -23,15 +23,15 @@ beta_estimates = apply(beta_post, 2, function(x) quantile(x, c(0.025, 0.975)))
 beta_means = colMeans(beta_post)
 
 y_grid = c(1:nrow(beta_post))
-par(mfrow = c(1,1))
+par(mfrow = c(3,3))
 for (i in 1:length(beta_true))
 {
   max_value = max(max(beta_post[, i]), beta_true[i], beta_means[i])
   min_value = min(min(beta_post[, i]), beta_true[i], beta_means[i])
-  plot(y_grid, beta_post[, i], type = "l", ylim = c(-1.5*abs(min_value), 1.5*max_value), main = paste("Plot of beta[, ", i, "]"), xlab = "MCMC iteration",
-       ylab = "beta estimates", xlim = c(10, nrow(beta_post) - 10))
-  abline(h = beta_true[i], col = "red")
-  abline(h = beta_means[i], col = "blue")
+  plot(y_grid, beta_post[, i], type = "l", ylim = c(-1*abs(min_value), 1*max_value), main = paste("Plot of beta[, ", i, "]"), xlab = "MCMC iteration",
+       ylab = "beta estimates", xlim = c(10, nrow(beta_post) - 10), col = "gray")
+  abline(h = beta_true[i], col = "darkgreen", lwd = 2)
+  abline(h = beta_means[i], col = "red", lty = 2, lwd = 2)
   
   # adding confience intervals
   high = beta_estimates[2, i]
@@ -41,13 +41,17 @@ for (i in 1:length(beta_true))
   abline(h = low, col = "darkgreen", lty = 3)
   # polygon(c(y_grid, rev(y_grid)), c(rep(high, length(y_grid)), rev(rep(low, length(y_grid)))),  
   #         col = yarrr::transparent("darkgreen", trans.val = .9), border = NA)
-  
-  legend("topleft", legend=c("Actual Beta Value", "Posterior Mean", "95% CI"), col = c("red", "blue", "darkgreen"), 
-         lty=c(1, 1, 3), cex = 0.6)
+  # 
+  # legend("topleft", legend=c("Actual Beta Value", "Posterior Mean", "95% CI"), col = c("red", "blue", "darkgreen"), 
+  #        lty=c(1, 1, 3), cex = 0.4, inset=c(-0.2,0))
 }
 
 
+par(mfrow = c(1,1))
+plot(sort(beta_true[-1]), sort(beta_means[-1]), type = "l")
 
-# plot(beta_actual, beta_means, type = "l")
+plot(sort(beta_true), sort(beta_means), type = "l")
+
+
 
 
