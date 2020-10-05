c56dc8e68849956c5ea2c26e3e5961e514435c8c
rshudde <rachael.shudde@gmail.com>
2020-10-05 14:22:44 -0500

    changed l_k and sigma_2 to be paramaters in data generation

--- R/main.R
+++ R/main.R
@@ -7,16 +7,19 @@ source('R/FUNC_Gibbs_Sampler.R')
 # initial estimates for paramaters outside loop
 # get data
 n_covariates = 10
-n_datasets = 50
+n_datasets = 30
 length_out = floor(n_covariates*n_datasets / 3)
 
 knots = seq(0, 1, length.out = length_out) # suggested to be n_covariates*n_datasets / 2
+l_k_start = 2
+sigma_2_start = 0.5
 
 # generate data
-data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covariates, knots = knots)
+data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covariates, knots = knots,  l_k = l_k_start, 
+                                sigma_2 = sigma_2_start)
 
 # run gibbs sampler
-results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 5000, write = TRUE, lb_0 = 0.1, lk_0 = 0.1, burn_in = 0.5)
+results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 2000, write = TRUE, lb_0 = 0.1, lk_0 = 0.1, burn_in = 0.3)
 
 
 
