02269ff25d3bfd43c312d62ed0598f56a263d006
rshudde <rachael.shudde@gmail.com>
2020-10-07 10:30:55 -0500

    added plot function calls

--- R/main.R
+++ R/main.R
@@ -3,11 +3,12 @@ source('R/FUNC_woodchan_samples.R')
 source('R/FUNC_paramater_estimates.R')
 source('R/DATA_generate_simulation.R')
 source('R/FUNC_Gibbs_Sampler.R')
+source('R/PLOTS_Gibbs_sampler.R')
 
 # initial estimates for paramaters outside loop
 # get data
-n_covariates = 5
-n_datasets = 20
+n_covariates = 6
+n_datasets = 30
 length_out = floor(n_covariates*n_datasets / 3)
 
 knots = seq(0, 1, length.out = length_out) # suggested to be n_covariates*n_datasets / 2
@@ -19,7 +20,14 @@ data = generate_simulation_data(n_datasets = n_datasets, n_covariates = n_covari
                                 sigma_2 = sigma_2_start)
 
 # run gibbs sampler
-results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 500, write = TRUE, lb_0 = 0.1, lk_0 = 0.1, burn_in = 0.3)
+results = gibbs_sampler(data_gibbs = data, knots_gibbs = knots, B = 1000, write = TRUE, lb_0 = 0.1, lk_0 = 0.1, burn_in = 0.3)
 
 
+
+###################
+plot_beta(data, results)
+plot_sigma(data, results)
+plot_lk(data, results)
+plot_mu(data, results)
+plot_xi(data, results)
 
