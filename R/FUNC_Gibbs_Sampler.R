7e2bf4030e51018cab68e95a33e3d70cee0ffb5f
rshudde <rachael.shudde@gmail.com>
2020-11-24 10:31:51 -0600

    commented out hardcoding

--- R/FUNC_Gibbs_Sampler.R
+++ R/FUNC_Gibbs_Sampler.R
@@ -3,24 +3,24 @@ library(invgamma)
 library(MASS)
 
 # functin to do Gibbs sampling 
-gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000,
+gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000, xi_initial,
                          lk_0 = 0.1, lb_0 = 0.1, a = 10^(-3), b = 10^(-3), sigma_mu = 100, alpha_mu = 0,
                          burn_in = 0.3, write = FALSE)
 {
   set.seed(1)
 
   ## stuff for debugging
-  B = 1000
-  lk_0 = 0.1
-  lb_0 = 0.1
-  a = 10^(-3)
-  b = 10^(-3)
-  sigma_mu = 100 # TODO fix naming here
-  alpha_mu = 0
-  burn_in = 0.3
-  write = FALSE
-  data_gibbs = data
-  knots_gibbs = knots
+  # B = 1000
+  # lk_0 = 0.1
+  # lb_0 = 0.1
+  # a = 10^(-3)
+  # b = 10^(-3)
+  # sigma_mu = 100 # TODO fix naming here
+  # alpha_mu = 0
+  # burn_in = 0.3
+  # write = FALSE
+  # data_gibbs = data
+  # knots_gibbs = knots
   
   # get X and y values from the data
   X = data_gibbs$X
@@ -46,13 +46,14 @@ gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000,
   xi_gibbs = matrix(rep(0, B * length(knots_gibbs)), nrow = B, ncol = length(knots_gibbs))
   mu_gibbs = matrix(rep(0, B * n_datasets), nrow = B, ncol = n_datasets)
 
-  xi_gibbs[1, ] = rnorm(length(knots_gibbs), 0, 1) # length of knots
+  xi_gibbs[1, ] = xi_initial # length of knots
   alpha_gibbs[1, ] = rep(1, n_covariates)
   alpha_0_gibbs = rep(1, n_covariates)
   beta_gibbs[1, ] = alpha_0_gibbs / sqrt(sum(alpha_0_gibbs^2))
   
   lb_gibbs = as.vector(lb_0)
   lk_gibbs = as.vector(lk_0)
+
   ################################################################################################################
   ################################################################################################################
   
@@ -73,13 +74,13 @@ gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000,
       K_gibbs[[i]] = get_K_i(sigma_2_gibbs[idx-1], M_gibbs[[i]])
       V_gibbs[[i]] = get_V_i(sigma_2_gibbs[idx-1], M_gibbs[[i]], K_gibbs[[i]])
     }
-    
+
     # getting beta
     alpha_gibbs[idx, ] = get_alpha(alpha_gibbs[idx - 1, ], y, mu_gibbs[idx-1, ], X, xi_gibbs[idx-1, ], knots_gibbs,
                                    sigma_2_gibbs[idx-1], lk_gibbs[idx-1], M_gibbs, K_gibbs)
     beta_gibbs[idx, ] = alpha_gibbs[idx, ] / sqrt(sum(alpha_gibbs[idx, ]^2))
-    
-    # getting mu
+
+    # # getting mu
     mu_temp = vector()
     g_gibbs = list()
     for (i in 1:nrow(y))
@@ -92,17 +93,25 @@ gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000,
       # print(paste("iteration: ", i, ": ", mu_temp[i]))
     }
     mu_gibbs[idx, ] = mu_temp
-    
+
     # getting sigma
     sigma_2_gibbs[idx] = get_sigma_squared(a_gibbs, b_gibbs, y, M_gibbs, mu_gibbs[idx, ], g_gibbs)
     
+    # mu_gibbs[idx, ] = mu_truth    
+    # sigma_2_gibbs[idx] = 0.5
+
     # getting xi
     xi_gibbs[idx, ] = get_xi(xi_gibbs[idx-1,], y, mu_gibbs[idx, ], X, beta_gibbs[idx, ], knots_gibbs,
                              sigma_2_gibbs[idx], lk_gibbs[idx-1], lb_gibbs[idx-1], M_gibbs, K_gibbs)
     
+    # xi_gibbs[idx,] = xi_initial
     # # get l_k and l_b
     lk_gibbs[idx] = get_lk(y, mu_gibbs, g_gibbs, sigma_2_gibbs[idx], lk_gibbs[idx-1]) # should just be passing it mu
     lb_gibbs[idx] = get_lb(y, lb_gibbs[idx-1], xi_gibbs[idx, ])
+    
+    # lk_gibbs[idx] = 4
+    # lb_gibbs[idx] = 0.1
+
   
     # print statement for time  
     if (idx %% 50 == 0) print(paste("iteration:", idx, "in", round(Sys.time() - start_inner, 2)))
@@ -111,7 +120,7 @@ gibbs_sampler = function(data_gibbs, knots_gibbs, B = 1000,
   
   
   # get burnin and remove 
-  burn_in = floor(B*burn_in)
+  burn_in = floor(B * burn_in)
   
   beta_gibbs = beta_gibbs[-c(1:burn_in), ]
   mu_gibbs = mu_gibbs[-c(1:burn_in), ]
