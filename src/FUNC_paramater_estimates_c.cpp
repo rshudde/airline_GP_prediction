ba0fc0f86c52f17197262e26df213ce527e14cab
rshudde <rachael.shudde@gmail.com>
2021-04-21 11:07:10 -0500

    change lb inverse to .i() from inv

--- src/FUNC_paramater_estimates_c.cpp
+++ src/FUNC_paramater_estimates_c.cpp
@@ -112,7 +112,7 @@ arma::vec get_mu_c(const arma::mat& y, const int n_datasets, const List g, MatLi
     arma::mat term_one = y.cols(indices); // get time_idx indices of the matrix
     arma::rowvec term_two = term_one.row(i); // get the specific row
 
-    arma::uvec g_temp = g(i); // get g[i]
+    arma::vec g_temp = g(i); // get g[i]
 
     arma::vec term_four = term_two.t() - g_temp; // y - g
 
@@ -122,8 +122,9 @@ arma::vec get_mu_c(const arma::mat& y, const int n_datasets, const List g, MatLi
     float term_seven = term_six(0,0);
 
     float alpha_mu_post_i = sigma_2_mu_post_i*(alpha_mu/sigma_2_mu + term_seven);
-    mu[i] = std::sqrt(sigma_2_mu_post_i) * (arma::randn()) + alpha_mu_post_i;
 
+    mu[i] = std::sqrt(sigma_2_mu_post_i) * arma::randn() + alpha_mu_post_i;
+    
     // std::cout << i << std::endl;
   }
 
@@ -149,40 +150,68 @@ arma::mat get_H_matrix_c(arma::vec w, arma::vec knots, int n_Knots)
 
 // [[Rcpp::export]]
 Rcpp::List psi_alpha_c(arma::vec alpha, const arma::mat& y, const int n_datasets,
-                       const List time_idx, MatList data, arma::vec mu, arma::vec xi,
+                       const List time_idx, Rcpp::List data, arma::vec mu, arma::vec xi,
                        MatList V_mat, arma::vec knots, const int n_Knots)
 {
   // step to get the new beta
-  float denomonator = 0;
+  float denomonator = 0.0;
   for (int i = 0; i < alpha.n_elem; i++)
   {
     denomonator += std::pow(alpha[i], 2);
   }
 
   arma::vec beta = arma::zeros<arma::vec>(alpha.n_elem);
-  for (int i = 0; i <alpha.n_elem; i++)
+  for (int i = 0; i < alpha.n_elem; i++)
   {
-    beta[i] = alpha[i]/denomonator;
+    beta[i] = alpha[i]/std::sqrt(denomonator); 
   }
-
+  
   Rcpp::List w(n_datasets);
   Rcpp::List H_mat(n_datasets);
   Rcpp::List g(n_datasets);
   
-  int negloglhood = 0;
+  float negloglhood = 0.0;
 
   for (int i = 0; i < n_datasets; i++)
   {
     // get data
     // // extract matrix we are inverting
-    Rcpp::NumericMatrix data_temp = data[i];
-    arma::mat data_i = Rcpp::as<arma::mat>(wrap(data_temp));
-    w[i] = (data_i.t() * beta + 1)/2;
+    arma::mat data_i = data[i];
+    arma::vec ones(data_i.n_rows);
+    ones.fill(1.0);
+
+    w[i] = ((data_i * beta) + ones)/2;
+    
     H_mat[i] = get_H_matrix_c(w[i], knots, n_Knots);
 
     Rcpp::NumericMatrix h_temp = H_mat[i];
     arma::mat h_i = Rcpp::as<arma::mat>(wrap(h_temp));
     g[i] = h_i * xi;
+    
+    // getting negative loglikelihood
+    Rcpp::NumericMatrix V_temp = V_mat[i];
+    arma::mat V_i = Rcpp::as<arma::mat>(wrap(V_temp));
+    
+    // get time indices
+    arma::uvec indices(as<arma::uvec>(wrap(time_idx(i))));
+    // subtract 1 from indices
+    for (int k = 0; k < indices.n_elem; k++)
+    {
+      indices[k] = indices[k] - 1;
+    }
+    
+    arma::mat y_row = y.cols(indices); // get time_idx indices of the matrix
+    arma::rowvec y_temp = y_row.row(i); // get the specific row
+    
+    // get y - mu[i]- g[[i]]
+    arma::vec mu_temp = arma::ones<arma::vec>(y_temp.n_elem) * mu[i];
+    mu_temp.fill(mu[i]);
+    arma::vec g_temp = g(i);
+    arma::rowvec term_two = y_temp - mu_temp.t() - g_temp.t();
+    
+    arma::mat temp_lik = term_two * arma::inv(V_i) * term_two.t();
+    
+    negloglhood += temp_lik(0,0);
   }
   
   return Rcpp::List::create( Rcpp::Named("negloglhood") = negloglhood/2, 
@@ -199,26 +228,27 @@ Rcpp::List get_alpha_c(arma::vec alpha_0, const arma::mat& y, const int n_datase
                        float c_prior = 1000.0)
 {
   // step one 
-  float theta = (2*M_PI)* arma::randu();
+  float theta = (2.0*M_PI) * arma::randu();
   arma::vec alpha_prior = c_prior * (arma::randn(n_covariates));
   arma::vec alpha_proposed = std::cos(theta) * alpha_0 + std::sin(theta) * alpha_prior;
   
   // step two 
-  float theta_min = theta - 2 * M_PI;
+  float theta_min = theta - 2.0 * M_PI;
   float theta_max = theta;
   
   // old negative loglikelihood
-  Rcpp::List psi_out_old = psi_alpha_c(alpha_0, y, n_datasets, time_idx, data,mu, xi, V_mat, knots, n_Knots);
-    
+  Rcpp::List psi_out_old = psi_alpha_c(alpha_0, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
+  Rcpp::List psi_out_new = psi_alpha_c(alpha_0, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
+  
   // new negative loglikelihood
-  float acceptance = -10.0;
+  float acceptance = 0.0;
   if (alpha_proposed[0] <= 0.0)
   {
       acceptance = 0.0;
   } 
   else
   {
-    Rcpp::List psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
+    psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
     
     // calculate new acceptance value
     float old_loglike = psi_out_old["negloglhood"];
@@ -226,7 +256,6 @@ Rcpp::List get_alpha_c(arma::vec alpha_0, const arma::mat& y, const int n_datase
     float value_new = exp(old_loglike - new_loglike);
     acceptance = std::fmin(1.0, value_new);
   }
-
   
   // step 3
   float zeta = randu();
@@ -259,7 +288,7 @@ Rcpp::List get_alpha_c(arma::vec alpha_0, const arma::mat& y, const int n_datase
     } 
     else
     {
-      Rcpp::List psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
+      psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
      
       // calculate new acceptance value
       float old_loglike = psi_out_old["negloglhood"];
@@ -271,7 +300,7 @@ Rcpp::List get_alpha_c(arma::vec alpha_0, const arma::mat& y, const int n_datase
   }
   
   // for return purposes
-  Rcpp::List psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
+  // Rcpp::List psi_out_new = psi_alpha_c(alpha_proposed, y, n_datasets, time_idx, data, mu, xi, V_mat, knots, n_Knots);
 
   return Rcpp::List::create( Rcpp::Named("negloglhood") = psi_out_new["negloglhood"], 
                              Rcpp::Named("beta") = psi_out_new["beta"],
@@ -437,7 +466,7 @@ float lk_acceptance_c(const arma::mat& y, const arma::vec mu, const List g, cons
   float ratio = 1.0;
   
   // indicator function part
-  if (lk_prime < 0.1 || lk_prime > 1 || l_k < 0.1 ||  l_k > 1)
+  if (lk_prime < 0.1 || lk_prime > 1.0 || l_k < 0.1 ||  l_k > 1.0)
   {
     to_return = 0.0;
   }
@@ -506,30 +535,30 @@ float lk_acceptance_c(const arma::mat& y, const arma::vec mu, const List g, cons
 }
 
 // [[Rcpp::export]]
-float lb_acceptance_c(const arma::mat y, const float lb, const float lb_prime, const arma::vec xi)
+float lb_acceptance_c(const arma::mat y, const float lb, const float lb_prime, const arma::vec xi, const arma::vec knots)
 {
   float to_return = 0.0;
-  if (lb_prime < 0.1 || lb_prime > 1 || lb < 0.1 ||  lb > 1)
+  if (lb_prime < 0.1 || lb_prime > 1.0 || lb < 0.1 ||  lb > 1.0)
   {
-    to_return = 0;
+    to_return = 0.0;
   }
   else 
   {
     //  set term_one = xi 
-    arma:: vec term_one = xi;
+    arma::vec term_one = xi;
     
     // # stuff we need to calcualte v_i
-    arma::mat M_lb = get_matern_c(lb, xi);
-    arma::mat M_lb_prime = get_matern_c(lb_prime, xi);
+    arma::mat M_lb = get_matern_c(lb, knots);
+    arma::mat M_lb_prime = get_matern_c(lb_prime, knots);
       
     // use tinv to invert M here
-    arma::mat term_two = arma::inv(M_lb) - arma::inv(M_lb_prime);
+    arma::mat term_two = (M_lb.i()) - (M_lb_prime.i());
     
     
     // # matrix multiplication
     arma::mat matrix_part1 = term_one.t() * term_two;
     arma::mat matrix_part = matrix_part1 * term_one;
-    float ratio = exp(-0.5 * matrix_part(0,0));
+    float ratio = std::exp(-0.5 * matrix_part(0,0));
     
     to_return = std::fmin(1.0, (lb_prime / lb) * ratio);
   }
@@ -664,6 +693,30 @@ Rcpp::List get_xi_backend_c(const arma::vec xi_0, const float sigmaB_2, const ar
                              Rcpp::Named("g") = psi_out_new["g"]);  
 }
 
+// [[Rcpp::export]]
+Rcpp::List inner_gibbs_one(float sigma_2_post, Rcpp::List M_gibbs, int n_datasets)
+{
+  Rcpp::List K_gibbs(n_datasets);
+  Rcpp::List V_gibbs(n_datasets);
+  
+  for (int i = 0; i < n_datasets; i++)
+  {
+    // extract matrix we are inverting
+    Rcpp::NumericMatrix M_temp = M_gibbs[i];
+    arma::mat M = Rcpp::as<arma::mat>(wrap(M_temp));
+    
+    arma:: mat K_temp = get_K_i_c(sigma_2_post, M);
+    arma:: mat V_temp = get_V_i_c(sigma_2_post, K_temp);
+    
+    K_gibbs[i] = K_temp;
+    V_gibbs[i] = V_temp;
+  }
+  return Rcpp::List::create(  Rcpp::Named("K_gibbs") = K_gibbs,
+                              Rcpp::Named("V_gibbs") = V_gibbs);
+    
+}
+
+
 
 // Rcpp::sourceCpp("src/FUNC_paramater_estimates_c.cpp")
 
@@ -676,3 +729,11 @@ Rcpp::List get_xi_backend_c(const arma::vec xi_0, const float sigmaB_2, const ar
 /*** R
 timesTwo(42)
 */
+
+
+// [[Rcpp::export]]
+float blah(float mean, float sd)
+{
+  float b = std::sqrt(sd)*arma::randn() + mean;
+  return b;
+}
