48f168967f4d7e4bbb842d383a3eb42729087a84
rshudde <rachael.shudde@gmail.com>
2022-01-10 16:30:14 -0600

    added path

--- R/TEST_yang_meeting.R
+++ R/TEST_yang_meeting.R
@@ -16,10 +16,11 @@ source('R/PLOTS_Gibbs_Sampler.R')
 Rcpp::sourceCpp("src/FUNC_paramater_estimates_c.cpp")
 
 
-data = generate_simulation_data(n_datasets = 100, n_time = 80, n_covariates = 15, seed = 1, seed2 = 1)
+data = generate_simulation_data(n_datasets = 100, n_time = 80, n_covariates = 10, seed = 1, seed2 = 1)
 
 results = gibbs_sampler_r(data_gibbs = data, B = 1000, 
                           xi_initial = runif(length(data$w_true[[1]]), -1, 1), 
                           burn_in = 0.7, 
                           NNGP = FALSE, 
                           n_to_store = 100)
+save(results, file = "TESTRESULTS.rda")
