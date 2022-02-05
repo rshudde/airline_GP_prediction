4169c98816c87e8b83747ec5c6f78eddcb20324e
Ananya Roy Chowdhury <ananya.rc94@tamu.edu>
2022-02-05 14:44:58 -0600

    changed T to T_val

--- R/DATA_read_t20.R
+++ R/DATA_read_t20.R
@@ -18,26 +18,28 @@ Rcpp::sourceCpp("src/FUNC_paramater_estimates_c.cpp")
 read_data = function(i){
   X_initial = read.csv(file = paste0("t20/X_data_n100_t20_rep",i,".csv"), header = T)
   Y = read.csv(file = paste0("t20/Y_data_n100_t20_rep",i,".csv"), header = T)
-  T = 20
+  T_val = 20
   X = list()
   range = nrow(X_initial)/20
   k = 1
   for(j in 1:range){
-    X[[j]] = X_initial[(k:(j*T)), ]
-    k = (j*T)+1
+    X[[j]] = as.matrix(X_initial[(k:(j*T_val)), ])
+    k = (j*T_val)+1
     
   }
   
-  return(list("X" = X, "Y" = Y, "T" = T))
+  return(list("X" = X, "Y" = Y, "T_val" = T_val))
 }
 
+i = 5
 data = read_data(i)
 print("GOT DATA")
 results = gibbs_sampler_r(data_gibbs = data, B = 100000,
-                          xi_initial = runif(data$T, -1, 1),
+                          xi_initial = runif(data$T_val, -1, 1),
                           burn_in = 0.5,
                           NNGP = FALSE,
                           n_to_store = 20000)
 save(results, file = "TESTRESULTS100.rda")
 
 
+a = as.matrix(data$X[[1]])
