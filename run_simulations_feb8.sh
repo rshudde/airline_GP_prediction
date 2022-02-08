cd12a31ad1103ea940b7e8a850e099d79aea2847
rshudde <rachael.shudde@gmail.com>
2022-02-08 17:45:53 -0600

    output

--- run_simulations_feb8.sh
+++ run_simulations_feb8.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # now run all of the datasets for x = 20 to see how this goes yay
-for i in {1..15}; do \
+for i in {16..50}; do \
 first="--args r="
 second=" t=40 B_VAL=50000 STORE_VAL=20000"
 c="${first}$i ${second}"
 echo "${c}"
-nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH40.out &
+nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH40_2.out &
 done
