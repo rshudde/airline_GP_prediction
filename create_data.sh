80faf087354dc693417cb48272a5b86e2e24ccd3
rshudde <rachael.shudde@gmail.com>
2022-02-08 16:30:51 -0600

    this creates the data

---
+++ create_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p t20
+mkdir -p t40
+mkdir -p t60
+mkdir -p t80
+mkdir -p RESULTS
+mkdir -p output
+echo "made directories"
+
+# now create the data
+if find t20 -mindepth 1 -maxdepth 1 | read; then
+nohup R CMD BATCH R/DATA_subset_datasets.R R/DATA_subset_datasets.out &
+fi
