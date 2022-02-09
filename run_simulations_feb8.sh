ca83437bbf32ffb11c2682055af61e61d52b509e
rshudde <rachael.shudde@gmail.com>
2022-02-09 08:40:02 -0600

    t=80

--- run_simulations_feb8.sh
+++ run_simulations_feb8.sh
@@ -3,8 +3,8 @@
 # now run all of the datasets for x = 20 to see how this goes yay
 for i in {1..25}; do \
 first="--args r="
-second=" t=60 B_VAL=10000 STORE_VAL=1000"
+second=" t=80 B_VAL=10000 STORE_VAL=1000"
 c="${first}$i ${second}"
 echo "${c}"
-nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH60.out &
+nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH80.out &
 done
--- run_simulations_feb8_pt2.sh
+++ run_simulations_feb8_pt2.sh
@@ -3,8 +3,8 @@
 # now run all of the datasets for x = 20 to see how this goes yay
 for i in {26..50}; do \
 first="--args r="
-second=" t=60 B_VAL=10000 STORE_VAL=1000"
+second=" t=80 B_VAL=10000 STORE_VAL=1000"
 c="${first}$i ${second}"
 echo "${c}"
-nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH60_2.out &
+nohup R CMD BATCH --no-save --no-restore "${first}$i ${second}" BLAH.R BLAH80_2.out &
 done
