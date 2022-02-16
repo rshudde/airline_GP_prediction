538ca9f4c269539f3d3dae892d1a3f838f9419c1
rshudde <rachael.shudde@gmail.com>
2022-02-16 14:08:36 -0600

    n = 10

--- analyze.sh
+++ analyze.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # delete the single folder
-Tnum=100
+Tnum=10
+n_replicates=50
 rm -r t$Tnum
 
 # do analysis 
-nohup R CMD BATCH --no-save --no-restore "--args t_vals=$Tnum" ANALYZE_simulations_single.R OUTPUT_analyzing.out &
+nohup R CMD BATCH --no-save --no-restore "--args t_vals=$Tnum n_replicates=$n_replicates" ANALYZE_simulations_single.R OUTPUT_analyzing.out &
 wait $BACK_PID 
 
 # delete the .rda files 
--- main_run.sh
+++ main_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-Tnum=100
+Tnum=10
 NNGP="FALSE"
+n_replicates=50
 MCMCiterations=50000
 nsave=2000
 
@@ -12,12 +13,12 @@ mkdir -p output
 
 # populate the folder if it's not empty
 if ! [ "$(ls -A t$Tnum)" ]; then
-	nohup R CMD BATCH --no-save --no-restore "--args t_vals=$Tnum" R/DATA_subset_datasets.R OUTPUT_creating_data.out &
+	nohup R CMD BATCH --no-save --no-restore "--args t_vals=$Tnum n_replicates=$n_replicates" R/DATA_subset_datasets.R OUTPUT_creating_data.out &
 fi
 wait $BACK_PID 
 
 # run the Gibbs samplers (loop over 50)
-for i in {1..50}; do \
+for i in $(seq 1 $n_replicates); do \
 nohup R CMD BATCH --no-save --no-restore "--args r=$i t=$Tnum B_VAL=$MCMCiterations STORE_VAL=$nsave" main_R_file.R OUTPUT_$Tnum.out &
 echo "--args r=$i t=$Tnum B_VAL=$MCMCiterations STORE_VAL=$nsave"
 done
