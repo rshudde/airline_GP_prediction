9fb5dc5054e2533967082228e4e583a961bb60af
rshudde <rachael.shudde@gmail.com>
2022-02-16 14:09:03 -0600

    cleaning up some plots

--- plots/airline_plots_normal_varying_t.R
+++ plots/airline_plots_normal_varying_t.R
@@ -1,52 +1,55 @@
 #setwd("C:\\Users\\anany\\Downloads\\NNGP\\NNGP")
-setwd("C:\\Users\\anany\\Desktop\\Research\\Flight_Delay\\output")
 library(ggplot2)
 
 #read in the data for beta
-beta_20 = read.csv("beta_20.csv")
-beta_40 = read.csv("beta_40.csv")
-beta_60 = read.csv("beta_60.csv")
-beta_80 = read.csv("beta_80.csv")
-#beta_100 = read.csv("beta_100.csv")
+beta_20 = read.csv("/Users/rachaelshudde/Desktop/output/beta_20.csv")
+beta_40 = read.csv("/Users/rachaelshudde/Desktop/output/beta_40.csv")
+beta_60 = read.csv("/Users/rachaelshudde/Desktop/output/beta_60.csv")
+beta_80 = read.csv("/Users/rachaelshudde/Desktop/output/beta_80.csv")
+beta_100 = read.csv("/Users/rachaelshudde/Desktop/output/beta_100.csv")
 
-
-beta_20_truth = read.csv("beta_truth_20.csv")
-beta_40_truth = read.csv("beta_truth_40.csv")
-beta_60_truth = read.csv("beta_truth_60.csv")
-beta_80_truth = read.csv("beta_truth_80.csv")
-#beta_100_truth = read.csv("beta_truth_100.csv")
-
+beta_20_truth = read.csv("/Users/rachaelshudde/Desktop/output/beta_truth_20.csv")
+beta_40_truth = read.csv("/Users/rachaelshudde/Desktop/output/beta_truth_40.csv")
+beta_60_truth = read.csv("/Users/rachaelshudde/Desktop/output/beta_truth_60.csv")
+beta_80_truth = read.csv("/Users/rachaelshudde/Desktop/output/beta_truth_80.csv")
+beta_100_truth = read.csv("/Users/rachaelshudde/Desktop/output/beta_truth_100.csv")
 
 #calculate the consistancy measure for beta
 beta20 = apply(as.matrix(abs(beta_20) - abs(beta_20_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_20_truth))
 beta40 = apply(as.matrix(abs(beta_40) - abs(beta_40_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_40_truth))
 beta60 = apply(as.matrix(abs(beta_60) - abs(beta_60_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_60_truth))
 beta80 = apply(as.matrix(abs(beta_80) - abs(beta_80_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_80_truth))
-#beta100 = apply(as.matrix(abs(beta_100) - abs(beta_100_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_100_truth))
+beta100 = apply(as.matrix(abs(beta_100) - abs(beta_100_truth)),1, function(x){sqrt(sum(x^2))})/sqrt(ncol(beta_100_truth))
 
 
 
 #make the violin plots for beta
-t = rep(c(20, 40, 60, 80), each = 50)
-beta_mat = data.frame(c(beta20, beta40, beta60, beta80),t)
+t_vals = rep(c(20, 40, 60, 80, 100), each = 50)
+beta_mat = data.frame(c(beta20, beta40, beta60, beta80, beta100),t_vals)
 colnames(beta_mat) = c("beta", "t")
 beta_mat$t = as.factor(beta_mat$t)
 
-#a = c(5,50)
-b = c(36, 37, 38, 45, 46)
-c = c(1,  4,  5,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-      23, 25, 26, 27, 28, 29, 30, 32, 33, 34, 37, 38, 40, 41, 42, 44, 45, 47, 48, 49, 50)
-idx = c(b+50, c+150)
-
-b = ggplot(beta_mat[-idx, ], aes(x = t, y = beta, color = t)) + geom_boxplot(width = 0.5) +ggtitle("Boxplot for beta (normal)");b
+beta_plot = ggplot(beta_mat, aes(x = t_vals, y = beta, color = t_vals)) + geom_boxplot(width = 0.5) +ggtitle("Boxplot for beta (normal)");b
 
 #save the plot for beta
-png(file="..\\boxplot_beta_varying_t.png", width=600, height=350)
-b
-dev.off()
+# png(file="..\\boxplot_beta_varying_t.png", width=600, height=350)
+# beta_plot
+# dev.off()
 
 
 #read in the data for gmu
+gmu_20 = read.csv("/Users/rachaelshudde/Desktop/output/gmu_20.csv")
+gmu_40 = read.csv("/Users/rachaelshudde/Desktop/output/gmu_40.csv")
+gmu_60 = read.csv("/Users/rachaelshudde/Desktop/output/gmu_60.csv")
+gmu_80 = read.csv("/Users/rachaelshudde/Desktop/output/gmu_80.csv")
+gmu_100 = read.csv("/Users/rachaelshudde/Desktop/output/gmu_100.csv")
+
+gmu_20_truth = read.csv("/Users/rachaelshudde/Desktop/output/gmu_truth_20.csv")
+gmu_40_truth = read.csv("/Users/rachaelshudde/Desktop/output/gmu_truth_40.csv")
+gmu_60_truth = read.csv("/Users/rachaelshudde/Desktop/output/gmu_truth_60.csv")
+gmu_80_truth = read.csv("/Users/rachaelshudde/Desktop/output/gmu_truth_80.csv")
+gmu_100_truth = read.csv("/Users/rachaelshudde/Desktop/output/gmu_truth_100.csv")
+
 gmu_20 = read.csv("gpmu_20.csv")
 gmu_40 = read.csv("gpmu_40.csv")
 gmu_60 = read.csv("gpmu_60.csv")
