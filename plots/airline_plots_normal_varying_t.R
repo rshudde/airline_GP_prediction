ea5fd3052d4a493131a85cb6719d9a93fa6950b1
Ananya Roy Chowdhury <ananya.rc94@tamu.edu>
2022-02-23 10:15:18 -0600

    added if statement for NNGP or FUll model

--- plots/airline_plots_normal_varying_t.R
+++ plots/airline_plots_normal_varying_t.R
@@ -53,29 +53,61 @@ beta_mat$t = factor(beta_mat$t, levels = unique(gmu_mat$t))
 gmu_mat$t = factor(gmu_mat$t, levels = unique(gmu_mat$t))
 sigma_mat$t = factor(sigma_mat$t, levels = unique(gmu_mat$t))
 
+# #create lable
+# lable = ifelse(NNGP,"NNGP", "Full")
+
 
 ## actually creating plots
-beta_plot = ggplot(beta_mat, aes(x = t, y = beta, color = t)) + geom_boxplot(width = 0.5) +
-  ggtitle(expression("Consistancy of function estimation for"~beta~"(NNGP Sampler)")) +
-  xlab("Time points") + ylab ("Norm Distance") + 
-  guides(color=guide_legend(title="Time points"))+ theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5))   #ylab(expression("||"~beta~"||"^{2}))
 
-g_plot = ggplot(gmu_mat, aes(x = t, y = gmu, color = t)) + geom_violin(trim=FALSE) + 
-  geom_boxplot(width=0.1) + ggtitle(expression("Consistancy of function estimation for g +"~mu~"(NNGP Sampler)")) +
-  xlab("Time points") + ylab ("Norm Distance")+
-  guides(color=guide_legend(title="Time points"))+ theme_bw() +theme(plot.title = element_text(hjust = 0.5))  #ylab(expression("||"~g + mu~"||"^{2}))
+if(NNGP== TRUE){
+  beta_NNGP = ggplot(beta_mat, aes(x = t, y = beta, color = t)) + geom_boxplot(width = 0.5) +
+    ggtitle(expression("Consistancy of function estimation for"~beta~"(NNGP Sampler)")) +
+    xlab("Time points") + ylab ("Norm Distance") + 
+    guides(color=guide_legend(title="Time points"))+ theme_bw() + 
+    theme(plot.title = element_text(hjust = 0.5))   #ylab(expression("||"~beta~"||"^{2}))
+  
+  gmu_NNGP = ggplot(gmu_mat, aes(x = t, y = gmu, color = t)) + geom_violin(trim=FALSE) + 
+    geom_boxplot(width=0.1) + ggtitle(expression("Consistancy of function estimation for g +"~mu~"(NNGP Sampler)")) +
+    xlab("Time points") + ylab ("Norm Distance")+
+    guides(color=guide_legend(title="Time points"))+ theme_bw() +theme(plot.title = element_text(hjust = 0.5))  #ylab(expression("||"~g + mu~"||"^{2}))
+  
+  sigma_NNGP = ggplot(sigma_mat, aes(x = sigma, color = t)) + geom_density(trim = TRUE) +
+    ggtitle(expression("Density plot for"~sigma^{2}~"(NNGP Sampler)")) +  
+    geom_vline(xintercept = 0.25) + xlab("Time points") +  
+    ylab(expression(Density~of~sigma^{2}))+ guides(color=guide_legend(title="Time points")) + theme_bw()+
+    theme(plot.title = element_text(hjust = 0.5)) 
+  # display plots
+  beta_NNGP
+  g_NNGP
+  sigma_NNGP
+  
+  
+}else{
+  beta_full = ggplot(beta_mat, aes(x = t, y = beta, color = t)) + geom_boxplot(width = 0.5) +
+    ggtitle(expression("Consistancy of function estimation for"~beta~"(Full Sampler)")) +
+    xlab("Time points") + ylab ("Norm Distance") + 
+    guides(color=guide_legend(title="Time points"))+ theme_bw() + 
+    theme(plot.title = element_text(hjust = 0.5))   #ylab(expression("||"~beta~"||"^{2}))
+  
+  gmu_full = ggplot(gmu_mat, aes(x = t, y = gmu, color = t)) + geom_violin(trim=FALSE) + 
+    geom_boxplot(width=0.1) + ggtitle(expression("Consistancy of function estimation for g +"~mu~"(Full Sampler)")) +
+    xlab("Time points") + ylab ("Norm Distance")+
+    guides(color=guide_legend(title="Time points"))+ theme_bw() +theme(plot.title = element_text(hjust = 0.5))  #ylab(expression("||"~g + mu~"||"^{2}))
+  
+  sigma_full = ggplot(sigma_mat, aes(x = sigma, color = t)) + geom_density(trim = TRUE) +
+    ggtitle(expression("Density plot for"~sigma^{2}~"(Full Sampler)")) +  
+    geom_vline(xintercept = 0.25) + xlab("Time points") +  
+    ylab(expression(Density~of~sigma^{2}))+ guides(color=guide_legend(title="Time points")) + theme_bw()+
+    theme(plot.title = element_text(hjust = 0.5)) 
+  
+  
+  # display plots
+  beta_full
+  g_full
+  sigma_full
+  
+}
 
-sigma_plot = ggplot(sigma_mat, aes(x = sigma, color = t)) + geom_density(trim = TRUE) +
-  ggtitle(expression("Density plot for"~sigma^{2}~"(NNGP Sampler)")) +  
-  geom_vline(xintercept = 0.25) + xlab("Time points") +  
-  ylab(expression(Density~of~sigma^{2}))+ guides(color=guide_legend(title="Time points")) + theme_bw()+
-  theme(plot.title = element_text(hjust = 0.5)) 
-
-# display plots
-beta_plot
-g_plot
-sigma_plot
 
 
 
