410ecc9ab40e4e58088c02f8c9c1d9c14a4828b2
rshudde <rachael.shudde@gmail.com>
2022-05-02 11:52:38 -0500

    new obsrvations

--- run_wc.sh
+++ run_wc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-num_observations=100
-num_replications=20
-num_burnin=100
-num_mcmc=100
+num_observations=1000
+num_replications=50
+num_burnin=500
+num_mcmc=1000
 
 # do analysis 
 nohup R CMD BATCH --no-save --no-restore "--args num_observations=$num_observations num_replications=$num_replications num_burnin=$num_burnin num_mcmc=$num_mcmc" R/sandy_code/npreg-wc.R wc.out &
