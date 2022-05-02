1d3efa4cc604ed9f02afedc148be0217f0f5b3ef
rshudde <rachael.shudde@gmail.com>
2022-05-02 11:44:42 -0500

    new parameters

--- run_nngp.sh
+++ run_nngp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-num_observations=10
-num_replications=10
+num_observations=100
+num_replications=20
 
 # do analysis 
 nohup R CMD BATCH --no-save --no-restore "--args num_observations=$num_observations num_replications=$num_replications" R/sandy_code/npreg-nngp.R nngp.out &
