0bbd5ade89b6890ca47e87c992629028f24b8010
rshudde <rachael.shudde@gmail.com>
2022-05-06 11:30:12 -0500

    changed mcmc number

--- run_nngp.sh
+++ run_nngp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-num_observations=500
+num_observations=100
 num_replications=20
 num_burnin=$num_observations-$num_observations/4
-num_mcmc=1000
+num_mcmc=5000
 
 # do analysis 
 nohup R CMD BATCH --no-save --no-restore "--args num_observations=$num_observations num_replications=$num_replications num_burnin=$num_burnin num_mcmc=$num_mcmc" R/sandy_code/npreg-nngp.R nngp.out &
