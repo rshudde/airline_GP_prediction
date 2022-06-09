3e716bf6d120d0e69558db79e53c06c6721c30e8
rshudde <rachael.shudde@gmail.com>
2022-06-09 09:55:44 -0500

    for timing

--- R/GIBBS_sampler_real_data.R
+++ R/GIBBS_sampler_real_data.R
@@ -3,10 +3,6 @@ library(dplyr)
 library(stats)
 library(recipes)
 library(caret)
-
-save("X_list.rda")
-save("Z_list.rda")
-
 source('R/FUNC_woodchan_samples.R')
 source('R/FUNC_paramater_estimates.R')
 source('R/DATA_generate_simulation.R')
@@ -15,20 +11,28 @@ source('R/FUNC_Gibbs_Sampler_r.R')
 source('R/PLOTS_Gibbs_sampler.R')
 Rcpp::sourceCpp('src/FUNC_paramater_estimates_c.cpp')
 
+# load in the data
+load("X_list.rda")
+load("Z_list.rda")
+
+# start gibbs sampler
 beta_names = colnames(X_list[[1]])
 time_idx = apply(Z, 1, function(x) which(!is.na(x)))
 start = 1
-end = 10
+end = length(X_list)
 data_actual = list(y = Z[start:end,], X = X_list[start:end], time_idx = time_idx[start:end])
-# data_gibbs = data_actual; B = 100; n_to_store = 50; runif(ncol(data_actual$X[[1]]), -1, 1)
 results = gibbs_sampler_r(data_gibbs = data_actual, 
                           B = 1,
                           xi_initial = runif(ncol(data_actual$X[[1]]), -1, 1),
                           burn_in = 0.5,
                           NNGP = TRUE,
+                          nNeighbour = 20,
                           n_to_store = 50)
+# data_gibbs = data_actual; B = 100; n_to_store = 50; runif(ncol(data_actual$X[[1]]), -1, 1)
+save(results, file = "REAL_DATA_GIBBS.rda")
 
-save(results)
+
+##############################3 
 # get posterior g samples
 # g_s = list()
 # post_g = colMeans(results$g)
@@ -44,34 +48,34 @@ save(results)
 # }
 
 # #plots
-par(mfrow = c(3,3))
-plot(colMeans(results$beta), main = "Beta plots")
-plot(results$sigma_2, type = "l", main = "sigma_2 plot")
-plot(results$lB, type  = "l", main = "lb")
-plot(colMeans(results$mu), main = "mu plots")
-plot(results$loglhood, type = "l", main = "loglikelihood")
-plot(colMeans(results$xi), main = "xi results")
-plot(results$beta[,5], type = "l")
-plot(results$beta[,25], type = "l")
-plot(results$beta[,45], type = "l")
-par(mfrow = c(1,1)); plot(colMeans(results$g))
-
-
-betas = as.data.frame(beta_names)
-betas$beta_values = colMeans(round(results$beta, 3))
-betas = betas[order(abs(betas$beta_values), decreasing = TRUE),]
-betas
-
-# look at prediction values of 
-idx = sample(1:end, 1)
-Z2 = Z[idx,]
-Z2 = Z2[-which(is.na(Z2))]
-Z2_pred = colMeans(results$mu)[idx] + g_s[[idx]]
-pred = as.data.frame(cbind(Z2, Z2_pred))
-colnames(pred) = c("actual", "predicted")
-# pred$actual = exp(pred$actual) - adjustment
-# pred$predicted = exp(pred$predicted)
-par(mfrow = c(1,2)); plot(pred$actual, pred$predicted); plot(abs(pred$actual - pred$predicted))
-
+# par(mfrow = c(3,3))
+# plot(colMeans(results$beta), main = "Beta plots")
+# plot(results$sigma_2, type = "l", main = "sigma_2 plot")
+# plot(results$lB, type  = "l", main = "lb")
+# plot(colMeans(results$mu), main = "mu plots")
+# plot(results$loglhood, type = "l", main = "loglikelihood")
+# plot(colMeans(results$xi), main = "xi results")
+# plot(results$beta[,5], type = "l")
+# plot(results$beta[,25], type = "l")
+# plot(results$beta[,45], type = "l")
+# par(mfrow = c(1,1)); plot(colMeans(results$g))
+# 
+# 
+# betas = as.data.frame(beta_names)
+# betas$beta_values = colMeans(round(results$beta, 3))
+# betas = betas[order(abs(betas$beta_values), decreasing = TRUE),]
+# betas
+# 
+# # look at prediction values of 
+# idx = sample(1:end, 1)
+# Z2 = Z[idx,]
+# Z2 = Z2[-which(is.na(Z2))]
+# Z2_pred = colMeans(results$mu)[idx] + g_s[[idx]]
+# pred = as.data.frame(cbind(Z2, Z2_pred))
+# colnames(pred) = c("actual", "predicted")
+# # pred$actual = exp(pred$actual) - adjustment
+# # pred$predicted = exp(pred$predicted)
+# par(mfrow = c(1,2)); plot(pred$actual, pred$predicted); plot(abs(pred$actual - pred$predicted))
+# 
 
 # 
